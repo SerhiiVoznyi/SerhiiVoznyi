2469272ef706ebd5d214759250027b16c9bb0165
Serhii Voznyi <serhiivoznyi@gmail.com>
2026-02-08 14:33:53 +0100

    Created project for website

---
+++ website/vite.config.ts
@@ -0,0 +1,17 @@
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react-swc";
+
+// https://vite.dev/config/
+export default defineConfig({
+  plugins: [react()],
+  css: {
+    preprocessorOptions: {
+      scss: {
+        additionalData: `@import "./src/_mantine";`,
+      },
+    },
+  },
+  optimizeDeps: {
+    include: ["react-router-dom"],
+  },
+});
